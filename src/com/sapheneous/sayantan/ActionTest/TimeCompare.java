1690a450f3ae938f83bca57e6cf42f118e94d262
Sayantan Ghosh <me@localhost.localdomain>
2016-01-24 05:46:06 +0530

    Time Compare

---
+++ src/com/sapheneous/sayantan/ActionTest/TimeCompare.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.sapheneous.sayantan.ActionTest;
+
+import java.time.ZonedDateTime;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import org.apache.commons.net.ntp.NTPUDPClient; 
+import org.apache.commons.net.ntp.TimeInfo;
+
+/**
+ * @author me
+ *
+ */
+public class TimeCompare {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		//System.out.println(ZonedDateTime.now());
+		long sysTime = System.currentTimeMillis();
+		
+		String TIME_SERVER = "time-a.nist.gov";   
+        NTPUDPClient timeClient = new NTPUDPClient();
+        InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
+        TimeInfo timeInfo = timeClient.getTime(inetAddress);
+        long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
+        long diffTime = returnTime - sysTime;
+        
+        Date ntpTime = new Date(returnTime);
+        Date localTime = new Date(sysTime);
+        System.out.println("Time on local machine: " + localTime);
+        System.out.println("Time from " + TIME_SERVER + ": " + ntpTime);
+        
+        System.out.println("We have a time offset of " + diffTime + " milliseconds.\n\n\n");
+	}
+
+}
